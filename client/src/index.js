e57bcf0c74ea7251619f1e478efe4dad16e13596
Rko_8001 <yadwindersingh0705@gmail.com>
2023-04-01 00:43:31 +0530

    create .env

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Check from './Check';
+import NavBar1 from "./studentSide/NavBar1";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    {/* <App /> */}
+    {/* <Check/> */}
+    <NavBar1/>
   </BrowserRouter>
 );
