fab2ac1610b32a2c1d1385685fc2eda95e8d8c21
Rko_8001 <yadwindersingh0705@gmail.com>
2023-03-31 21:01:02 +0530

    added accounts section ui

---
+++ client/src/components_accounts/Info.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function BasicStack(props) {
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stack spacing={2}>
+        <Item><b>Accounts officer Email:</b> {props.email}</Item>
+        <Item><b>Name:</b> Adish</Item>
+        <Item><b>Date of Joining:</b> 12/09/20</Item>
+        {/* <Item>Item 2</Item> */}
+        {/* <Item>Item 3</Item> */}
+      </Stack>
+    </Box>
+  );
+}
