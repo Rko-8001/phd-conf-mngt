3e9ed8b1ab4588ada1533b3a238baf430327e76e
Rko_8001 <yadwindersingh0705@gmail.com>
2023-04-09 17:55:38 +0530

    adding user feature to research section

--- client/src/components_research/users/StudentUser.js
+++ client/src/components_research/users/StudentUser.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
 import ReactPaginate from 'react-paginate';
+import * as XLSX from 'xlsx';
 import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 function StudentUser({ users }) {
 
     const [currentPage, setCurrentPage] = useState(1);
-
-    const [userPerPage] = useState(10);
-
-    // ...
+    const [userPerPage] = useState(5);
 
     const indexOfLastUser = currentPage * userPerPage;
     const indexOfFirstUser = indexOfLastUser - userPerPage;
     const currentUsers = users.slice(indexOfFirstUser, indexOfLastUser);
+
+    const [fileName, setFileName] = useState("Student Data Excel File");
+    const [uploadStudent, setUploadStudent] = useState([]);
 
     const paginate = ({ selected }) => {
         setCurrentPage(selected + 1);
@@ -30,13 +31,50 @@ function StudentUser({ users }) {
         </tr>
     );
 
+    async function handleFile(e) {
+        setFileName(e.target.files[0].name);
+        const files = e.target.files[0];
+        const data = await files.arrayBuffer();
+
+        const workBook = XLSX.read(data);
+        const workSheet = workBook.Sheets[workBook.SheetNames[0]];
+        const studentData = XLSX.utils.sheet_to_json(workSheet);
+
+        setUploadStudent(studentData);
+
+    };
+
+    async function uploadFile(e) {
+        e.preventDefault();
+        setFileName("Uploading Student Data. Please Wait");
+        var data;
+        try {
+            const resp = await fetch("/addStudent", {
+                method: "POSt",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(uploadStudent)
+            });
+            data = await resp.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+        var wb = XLSX.utils.book_new();
+        var ws = XLSX.utils.json_to_sheet(data.update);
+        XLSX.utils.book_append_sheet(wb,ws, "updates");
+        XLSX.writeFile(wb, "studentUploadUpdates.xlsx");
+        setFileName("Data Uploaded..");
+        
+    }
 
     return (
         <>
             <section className="bg-white dark:bg-gray-900">
                 <div className=" px-4 mx-auto max-w-screen-xl  lg:py-6 lg:px-3">
-                    <div className="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
-                        <h2 className="mb-1 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white"> Student Info</h2>
+                    <div className="mx-auto max-w-screen-sm text-center ">
+                        <h2 className="mb-1 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white"> Student Info</h2>
                     </div>
 
                     <section className="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
@@ -92,6 +130,33 @@ function StudentUser({ users }) {
                             </div>
                         </div>
                     </section>
+                </div>
+
+
+                <div className=" px-4 mx-auto max-w-screen-xl  lg:py-6 lg:px-3">
+                    <div className="mx-auto max-w-screen-sm text-center ">
+                        <h2 className="mb-1 text-3xl tracking-tight font-extrabold text-gray-900 dark:text-white">Add Students</h2>
+                    </div>
+
+                    <div>
+                        <label for="file" className="block text-sm text-gray-500 dark:text-gray-300">File</label>
+
+                        <label for="dropzone-file" className="flex flex-col items-center w-full max-w-lg p-5 mx-auto mt-2 text-center bg-white border-2 border-gray-300 border-dashed cursor-pointer dark:bg-gray-900 dark:border-gray-700 rounded-xl">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8 text-gray-500 dark:text-gray-400">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
+                            </svg>
+                            <h2 className="mt-1 font-medium tracking-wide text-gray-700 dark:text-gray-200">{fileName}</h2>
+                            <p className="mt-2 text-xs tracking-wide text-gray-500 dark:text-gray-400">Upload or drag & drop your Excel File</p>
+                            <input id="dropzone-file" className="hidden" onChange={(e) => handleFile(e)} type="file" />
+                        </label>
+                    </div>
+                    <div className="mx-auto mt-3 max-w-screen-sm text-center ">
+                        <button
+                            onClick={uploadFile}
+                            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                            Upload Student Data
+                        </button>
+                    </div>
                 </div>
             </section>
         </>
--- server/router/researchSide.js
+++ server/router/researchSide.js
@@ -13,25 +13,24 @@ const AppData = require('../model/applicationData');
 
 
 // Approve or Disapprove Logic
-router.post('/researchApproveOrDisapprove', async (req , res) => {
+router.post('/researchApproveOrDisapprove', async (req, res) => {
     // const {id, status, remarks} = req.body;
-    const {id, status} = req.body;
-    
-                // Debug Purpose
+    const { id, status } = req.body;
+
+    // Debug Purpose
     // console.log(id);    console.log(status);
-    
+
     try {
-        
+
         const appData = await AppData.findById(id);
-        
-        if(appData.status !== "1")
-        {
+
+        if (appData.status !== "1") {
             return res.status(422).json("Can't Approve Or Disapprove..");
         }
-        
+
         // await AppData.findByIdAndUpdate(id, {status: status, remarksResearch: remarks});
-        await AppData.findByIdAndUpdate(id, {status: status});
-        
+        await AppData.findByIdAndUpdate(id, { status: status });
+
         //             Debug Purpose
         // const appData2 = await AppData.findById(id);
         // console.log(appData2.status);
@@ -42,23 +41,66 @@ router.post('/researchApproveOrDisapprove', async (req , res) => {
     }
 })
 
-router.get("/viewUsers" , async (req, res) => {
+// viewing users - student/ faculty
+router.get("/viewUsers", async (req, res) => {
 
     try {
         var role;
-        role = "0"; const studentUser = await User.find({role: role});
-        role = "1"; const facultyUser = await User.find({role: role});
+        role = "0"; const studentUser = await User.find({ role: role });
+        role = "1"; const facultyUser = await User.find({ role: role });
 
-        return res.status(200).json({studentUser: studentUser, facultyUser: facultyUser});
+        return res.status(200).json({ studentUser: studentUser, facultyUser: facultyUser });
 
     } catch (error) {
         console.log(error);
     }
 })
 
+// adding student
+router.post("/addStudent", async (req, res) => {
+    const data = req.body;
+    const updates = [];
+    for (const student of data) {
+        var name = student.name;
+        var email = student.email;
+        var entryNo = student.entryNo;
+        var department = student.department;
+        var dateOfJoining = student.dateOfJoining;
+        var fellowshipCategory = student.fellowshipCategory;
+        var areaOfSpecialisation = student.areaOfSpecialisation;
+        var nameOfSupervisor = student.nameOfSupervisor;
+        var emailOfSuperVisor = student.emailOfSuperVisor;
+        var role = "0";
+        var mssg = "";
+        try {
+            const user = await User.findOne({ email: email });
+            if (user) {
+                mssg = "Already Exist..";
+            }
+            else {
+                const newUser = new User({
+                    name, email, entryNo, dateOfJoining,
+                    department, fellowshipCategory, areaOfSpecialisation,
+                    nameOfSupervisor, emailOfSuperVisor, role
+                });
+                await newUser.save();
+                mssg = "user added.";
+                
+
+            }
+        } catch (error) {
+            mssg = "Error Occured while Adding";
+            console.log(error);
+        }
+        updates.push({
+            "name": name,
+            "email": email,
+            "remarks": mssg,
+        })
+    }
+    return res.status(200).json({update: updates});
+})
+
 
 
 module.exports = router;
-
-
-
