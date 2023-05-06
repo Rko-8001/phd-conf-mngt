2effd92629d2a509534aecbe233a1b856e48b2d1
Rko_8001 <yadwindersingh0705@gmail.com>
2023-05-06 22:54:31 +0530

    Minor Changes acc to remarks

--- client/src/components_login/LoginN.js
+++ client/src/components_login/LoginN.js
@@ -116,50 +116,15 @@ function Login() {
         setLoginButtonMssg("Log In.")
     }
 
-    const loginWithGoogle = async (e) => {
-        console.log("Hello");
-        e.preventDefault();
-        const res = await fetch("/auth", {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-                'Access-Control-Request-Method': 'GET, POST, DELETE, PUT, OPTIONS'
-            }
-        });
-
-        const data = await res.json();
-        const role = data.split(' ')[0];
-        const email = data.split(' ')[1];
-        console.log(email);
-        if (res.status === 200) {
-            if (role === "0") {
-                navigate('/studentLogin');
-            }
-            else if (role === "1") {
-                navigate('/facultyLogin');
-            }
-            else if (role === "2") {
-                navigate('/adminLogin');
-            }
-            else if (role === "3") {
-                navigate('/accountLogin');
-            }
-        }
-        else {
-            window.alert("Error! Try Again..");
-        }
-    }
 
 
     return (
         <>
 
-            <nav class="bg-gray-800 text-white flex justify-between items-center py-3 px-5">
-                <h1 class="text-xl font-bold">PCMP :: PhD Conference Management Portal</h1>
-                <div class="flex items-center space-x-5 text-xs">
-                    <ul class="flex space-x-10 text-gray-300">
+            <nav className="bg-gray-800 text-white flex justify-between items-center py-3 px-5">
+                <h1 className="text-xl font-bold">PhDCGM :: PhD Conference Grant Management Portal</h1>
+                <div className="flex items-center space-x-5 text-xs">
+                    <ul className="flex space-x-10 text-gray-300">
 
                         <li>
                             <Link to="/meetTheTeam">Team</Link>
@@ -171,66 +136,81 @@ function Login() {
                             <Link to="/contactUs">Contact Us</Link>
                         </li>
                     </ul>
-                    <div class="space-x-5">
+                    <div className="space-x-5">
                         <button onClick={(e) => {
                             e.preventDefault();
                             navigate('/login');
-                        }} class="border px-5 py-2 rounded font-bold">Login</button>
+                        }} className="border px-5 py-2 rounded font-bold">Login</button>
                     </div>
                 </div>
             </nav>
 
 
-            <section class="bg-gray-50 min-h-screen flex items-center justify-center">
-                <div class="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
-                    <div class="md:w-1/2 px-8 md:px-16">
-                        <h2 class="font-bold text-2xl text-[#002D74]">Login with PCMP credentials</h2>
-                        <p class="text-xs mt-4 text-[#002D74]">Please enter your email and OTP to Log In</p>
+            <section className="bg-gray-50 min-h-screen flex items-center justify-center">
+                <div className="bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center">
+                    <div className="md:w-1/2 px-8 md:px-16">
+                        <h2 className="font-bold text-2xl text-[#002D74]">Login with PhDCGM credentials</h2>
+                        <p className="text-xs mt-4 text-[#002D74]">Please enter your email and OTP to Log In</p>
 
-                        <form action="" class="flex flex-col gap-4">
-                            <input class="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email" onChange={getEmailId} />
+                        <form action="" className="flex flex-col gap-4">
+                            <input className="p-2 mt-8 rounded-xl border" type="email" name="email" placeholder="Email" onChange={getEmailId} />
                             {optButton
                                 ?
                                 <Alert mssg={otpMssg} type={alertType} />
                                 :
-                                <button class="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300 " onClick={requestOtp}>{optButtonMssg}</button>
+                                <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300 " onClick={requestOtp}>{optButtonMssg}</button>
                             }
 
-                            <div class="relative">
-                                <input class="p-2 rounded-xl border w-full" name="password" placeholder="OTP" onChange={getOtp} />
+                            <div className="relative">
+                                <input className="p-2 rounded-xl border w-full" name="password" placeholder="OTP" onChange={getOtp} />
                             </div>
-                            <button class="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300" onClick={requestLogIn}>{loginButtonMssg}</button>
+                            <button className="bg-[#002D74] rounded-xl text-white py-2 hover:scale-105 duration-300" onClick={requestLogIn}>{loginButtonMssg}</button>
                         </form>
 
-                        <div class="mt-6 grid grid-cols-3 items-center text-gray-400">
-                            <hr class="border-gray-400" />
-                            <p class="text-center text-sm">OR</p>
-                            <hr class="border-gray-400" />
+                        <div className="mt-6 grid grid-cols-3 items-center text-gray-400">
+                            <hr className="border-gray-400" />
+                            <p className="text-center text-sm">OR</p>
+                            <hr className="border-gray-400" />
                         </div>
 
-                        <button class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]" onClick={loginWithGoogle}>
-                            <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
+                        <button
+                            onClick={async (e) => {
+                                e.preventDefault();
+                                const res = await fetch(`${BASE_URL}/auth`, {
+                                    method: "GET",
+                                    headers: {
+                                        'Accept': 'application/json',
+                                        'Access-Control-Allow-Origin': '*',
+                                        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+                                        'Access-Control-Request-Method': 'GET'
+                                    }
+                                });
+                                console.log(res);
+                            }}
+                            className="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 text-[#002D74]"
+                        >
+                            <svg className="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
                                 <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
                                 <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
                                 <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z" />
                                 <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
                             </svg>
-                            Login with Google
+                            Login With Google
                         </button>
 
-                        {/* <div class="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
+                        {/* <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
         <a href="#">Forgot your password?</a>
       </div> */}
 
-                        {/* <div class="mt-3 text-xs flex justify-between items-center text-[#002D74]">
+                        {/* <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
         <p>Don't have an account?</p>
-        <button class="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Register</button>
+        <button className="py-2 px-5 bg-white border rounded-xl hover:scale-110 duration-300">Register</button>
       </div> */}
                     </div>
 
                     {/* <!-- image --> */}
-                    <div class="md:block hidden w-1/2">
-                        <img class="rounded-2xl" src={img} alt='iitrpr pic' />
+                    <div className="md:block hidden w-1/2">
+                        <img className="rounded-2xl" src={img} alt='iitrpr pic' />
                     </div>
                 </div>
             </section>
