80bc510166b092ce7082a5ee1fb8838b9dbc7cc1
Yadwinder Singh <103496316+Rko-8001@users.noreply.github.com>
2023-05-13 12:08:35 +0530

    ab rha ni jaata jldi crow

--- client/src/components/requests/URL.js
+++ client/src/components/requests/URL.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "https://phd-conf-mngt-backend.onrender.com"
 // export const BASE_URL = "http://localhost:5000"
-export const BASE_URL = "172.30.2.244";
+export const BASE_URL = "172.30.2.244:5000";
 
